086d99aec6e9efb860dac77cf46d9ec66e9433af
spences10 <spences10apps@gmail.com>
2025-12-15 20:13:33 +0000

    refactor: :recycle: use latest scaffold

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-	import '../app.css';
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
